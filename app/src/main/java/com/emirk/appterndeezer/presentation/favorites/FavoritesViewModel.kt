18e2ac50f7d63395a2e536a3feb2dcf375411acf
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-09 19:27:15 +0300

    Change fragment name

--- app/src/main/java/com/emirk/appterndeezer/presentation/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/emirk/appterndeezer/presentation/favorites/FavoritesViewModel.kt
@@ -1,10 +1,10 @@
-package com.emirk.appterndeezer.presentation.dashboard
+package com.emirk.appterndeezer.presentation.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
