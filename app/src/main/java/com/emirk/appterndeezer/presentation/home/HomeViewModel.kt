bf5b537870bc0b337f3e4a95cfd2220760401658
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-09 22:05:33 +0300

    Get categories on home screen

--- app/src/main/java/com/emirk/appterndeezer/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/emirk/appterndeezer/presentation/home/HomeViewModel.kt
@@ -1,10 +1,49 @@
 package com.emirk.appterndeezer.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.emirk.appterndeezer.common.Resource
+import com.emirk.appterndeezer.domain.use_case.GetCategoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getCategoryUseCase: GetCategoryUseCase,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
+    fun getCategories() = viewModelScope.launch {
+        getCategoryUseCase.invoke().collect { result ->
+            when (result) {
+                is Resource.Error -> {
+                    _uiState.update { state ->
+                        state.copy(userMessage = result.message)
+                    }
+                }
+                is Resource.Loading -> {
+                    _uiState.update { state ->
+                        state.copy(isLoading = true)
+                    }
+                }
+                is Resource.Success -> {
+                    _uiState.update { state ->
+                        state.copy(category = result.data, isLoading = false)
+                    }
+                }
+            }
+        }
+    }
+
+    fun userMessageShown() {
+        _uiState.update { currentUiState ->
+            currentUiState.copy(userMessage = null)
+        }
+    }
 }
