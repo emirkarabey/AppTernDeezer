d2319ca61276b3155919c886a6a6a98eae1e4d3c
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-10 15:43:25 +0300

    Create artist fragment and view model

---
+++ app/src/main/java/com/emirk/appterndeezer/presentation/artists/ArtistsViewModel.kt
@@ -0,0 +1,9 @@
+package com.emirk.appterndeezer.presentation.artists
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ArtistsViewModel @Inject constructor(
+) : ViewModel()
