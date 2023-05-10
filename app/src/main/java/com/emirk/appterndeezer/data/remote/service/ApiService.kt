a02f5223ca286d9b7030b41683010001fc6d918d
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-10 16:37:51 +0300

    Add get artist function in api service.

--- app/src/main/java/com/emirk/appterndeezer/data/remote/service/ApiService.kt
+++ app/src/main/java/com/emirk/appterndeezer/data/remote/service/ApiService.kt
@@ -1,12 +1,20 @@
 package com.emirk.appterndeezer.data.remote.service
 
-import com.emirk.appterndeezer.data.remote.dto.CategoryResponse
+import com.emirk.appterndeezer.data.remote.dto.artist.ArtistResponse
+import com.emirk.appterndeezer.data.remote.dto.category.CategoryResponse
+import com.emirk.appterndeezer.util.Constants.ARTISTS_END_POINT
 import com.emirk.appterndeezer.util.Constants.CATEGORY_END_POINT
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET(CATEGORY_END_POINT)
     suspend fun getCategories(
     ): CategoryResponse
+
+    @GET(ARTISTS_END_POINT)
+    suspend fun getArtists(
+        @Path("genre_id") genreId: String
+    ): ArtistResponse
 }
