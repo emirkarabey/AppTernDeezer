fd8183e801dead0a4949c5f3be059dbfa4709d99
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-10 22:08:59 +0300

    Add end points

--- app/src/main/java/com/emirk/appterndeezer/util/Constants.kt
+++ app/src/main/java/com/emirk/appterndeezer/util/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val DEEZER_BASE_URL = "https://api.deezer.com/"
     const val CATEGORY_END_POINT = "genre"
     const val ARTISTS_END_POINT = "genre/{genre_id}/artists"
+    const val ARTISTS_DETAIL_END_POINT = "artist/{artist_id}"
+    const val ARTISTS_ALBUM_END_POINT = "artist/{artist_id}/albums"
 }
