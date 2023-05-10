0b7c70c483f20ef97ccb9271cd14a7b6f00dbf87
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-10 16:40:46 +0300

    Add get artist function artist view model.

--- app/src/main/java/com/emirk/appterndeezer/presentation/artists/ArtistsViewModel.kt
+++ app/src/main/java/com/emirk/appterndeezer/presentation/artists/ArtistsViewModel.kt
@@ -1,9 +1,49 @@
 package com.emirk.appterndeezer.presentation.artists
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.emirk.appterndeezer.common.Resource
+import com.emirk.appterndeezer.domain.use_case.GetArtistUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ArtistsViewModel @Inject constructor(
-) : ViewModel()
+    private val getArtistUseCase: GetArtistUseCase
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(ArtistUiState())
+    val uiState: StateFlow<ArtistUiState> = _uiState.asStateFlow()
+
+    fun getArtists(genreId: String) = viewModelScope.launch {
+        getArtistUseCase.invoke(genreId).collect { result ->
+            when (result) {
+                is Resource.Error -> {
+                    _uiState.update { state ->
+                        state.copy(userMessage = result.message)
+                    }
+                }
+                is Resource.Loading -> {
+                    _uiState.update { state ->
+                        state.copy(isLoading = true)
+                    }
+                }
+                is Resource.Success -> {
+                    _uiState.update { state ->
+                        state.copy(artist = result.data, isLoading = false)
+                    }
+                }
+            }
+        }
+    }
+
+    fun userMessageShown() {
+        _uiState.update { currentUiState ->
+            currentUiState.copy(userMessage = null)
+        }
+    }
+}
